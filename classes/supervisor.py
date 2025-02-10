b1fb3d2f5fd8afacb3d56431ef1146dcbc49ad45
PierluT <piertartabini@gmail.com>
2025-02-10 15:12:18 +0100

    correct beat logic. Next step: implement beat change

--- classes/supervisor.py
+++ classes/supervisor.py
@@ -193,11 +193,12 @@ class Supervisor:
     # EVERY ROBOT UPDATES ITS GLOBAL SPARTITO TO BE CONSCIOUS OF WHAT HAS BEEN PLAYED.
     def update_global_robot_spartito(self, millisecond):
         for robot in self.dictionary_of_robots:
-            # proper global spartito update.
             robot.update_orchestra_spartito(self.conductor_spartito)
             robot.update_phase_kuramoto_model(millisecond)
-            
-    
+        
+        for robot in self.dictionary_of_robots:     
+            robot.update_beat_firefly(millisecond)
+
     def clean_robot_buffers(self):
         for robot in self.dictionary_of_robots:
             robot.clean_buffers() 
